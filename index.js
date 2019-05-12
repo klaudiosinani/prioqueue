3a8eef7092998805a49acf3d8940875f3261887e
Klaus Sinani <klaussinani@gmail.com>
2019-05-12 21:30:29 +0300

    Add `Queue` class to `index` module exports

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
 const Item = require('./src/item');
+const Queue = require('./src/queue');
 
-module.exports = Object.assign({}, {Item});
+module.exports = Object.assign({}, {Item}, {Queue});
